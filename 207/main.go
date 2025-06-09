ce4c3cf84c7020eaaab8641f60a85185cdb2a651
Andrew <41926744+AndreSci@users.noreply.github.com>
2025-06-09 12:01:18 +0300

    Create main.go
    WORKER POOL

---
+++ 207/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+
+func main() {
+  // Тема WORKER POOL
+	t := time.Now()
+	const jobsCount, workerCount = 15, 15
+
+	jobs := make(chan int, 15)
+
+	results := make(chan int, 15)
+
+	for i := range workerCount {
+		go worker(i + 1, jobs, results)
+	}
+
+	for i := range jobsCount {
+		jobs <- i +1
+	}
+
+	close(jobs)
+
+	for i := range jobsCount {
+		fmt.Printf("result #%d : value = %d\n", i+ 1, <-results)
+	}
+
+	
+	fmt.Println("TIME ELAPSED:", time.Since(t).String())
+}
+
+func worker(id int, jobs <- chan int, results chan<- int) {
+	for j := range jobs {
+		time.Sleep(time.Second)
+		fmt.Printf("worker #%d finished\n", id)
+		results <- j * j
+	}
+}
