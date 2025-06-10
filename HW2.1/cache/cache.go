e9333ee24c7f82eb4f312d0753035dc5171871af
Andrew <41926744+AndreSci@users.noreply.github.com>
2025-06-10 11:04:40 +0300

    Update cache.go

--- HW2.1/cache/cache.go
+++ HW2.1/cache/cache.go
@@ -9,37 +9,31 @@ import (
 
 // Обьявляем структуру
 type cache struct {
-	m map[string]int
+	m        map[string]int
 	expireAt map[string]time.Time
-	ttlHis map[string]time.Duration
-	mu sync.Mutex
+	ttlHis   map[string]time.Duration
+	mu       sync.Mutex
 }
 
 // Создаем образец структуры
 func New() *cache {
 	return &cache{
-		m: make(map[string]int),
+		m:        make(map[string]int),
 		expireAt: make(map[string]time.Time),
-		ttlHis: make(map[string]time.Duration),
+		ttlHis:   make(map[string]time.Duration),
 	}
 }
 
-// Добавляем элемент в массивы и при необходимости создаем горутину для самоудалении 
+// Добавляем элемент в массивы и при необходимости создаем горутину для самоудалении
 func (c *cache) Set(str string, value any, ttl time.Duration) {
 	switch id := value.(type) {
 	case int:
 		c.mu.Lock()
-
-		_, ok := c.m[str]
-
-		if !ok {
-			go c.timerDel(str)
-		}
 		c.m[str] = id
 		c.expireAt[str] = time.Now()
 		c.ttlHis[str] = ttl
 		c.mu.Unlock()
-		
+
 	default:
 		fmt.Println("Wrong type of id")
 	}
@@ -50,12 +44,14 @@ func (c *cache) Set(str string, value any, ttl time.Duration) {
 func (c *cache) Get(str string) (int, error) {
 
 	c.mu.Lock()
+	// Проверяем переменную
+	c.timerDel(str)
 	value, exist := c.m[str]
 	c.mu.Unlock()
 
 	if exist {
 		return value, nil
-	} 
+	}
 
 	return 0, errors.New("not found key")
 }
@@ -69,23 +65,14 @@ func (c *cache) Delete(str string) {
 	c.mu.Unlock()
 }
 
-// Функция для горутинны которая удаляет элемент
-func (c * cache) timerDel(str string) {
-	for {
-		time.Sleep(100 * time.Millisecond)
+// Функция которая удаляет элемент
+func (c *cache) timerDel(str string) {
 
-		c.mu.Lock()
+	ttl, ok := c.ttlHis[str]
 
-		ttl, ok := c.ttlHis[str]
-		
-		if ok && time.Now().After(c.expireAt[str].Add(ttl)) {
-			delete(c.m, str)
-			delete(c.expireAt, str)
-			delete(c.ttlHis, str)
-			c.mu.Unlock()
-			break
-		} else {
-			c.mu.Unlock()
-		}
+	if ok && time.Now().After(c.expireAt[str].Add(ttl)) {
+		delete(c.m, str)
+		delete(c.expireAt, str)
+		delete(c.ttlHis, str)
 	}
 }
