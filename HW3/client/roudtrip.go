b59adb8962111b75874fe9dab820ed61f2805b64
Andrew <41926744+AndreSci@users.noreply.github.com>
2025-06-18 11:55:29 +0300

    Create roudtrip.go

---
+++ HW3/client/roudtrip.go
@@ -0,0 +1,19 @@
+package client
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+)
+
+// Базовый Middleware для логгирования запросов
+type loggingRoundTripper struct {
+	logger io.Writer
+	next   http.RoundTripper
+}
+
+func (l loggingRoundTripper) RoundTrip(r *http.Request) (*http.Response, error) {
+	fmt.Fprintf(l.logger, "[%s] %s %sn", time.Now().Format(time.ANSIC), r.Method, r.URL)
+	return l.next.RoundTrip(r)
+}
